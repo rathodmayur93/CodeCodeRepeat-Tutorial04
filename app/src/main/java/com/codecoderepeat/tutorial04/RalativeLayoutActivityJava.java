94a0bcb5070beef712bda8e005114edf7bf71e6f
Mayur Rathod <rathodmayur93@gmail.com>
2018-07-10 23:46:33 +0530

    Added RelativeLayout XML, Kotlin & Java File

---
+++ app/src/main/java/com/codecoderepeat/tutorial04/RalativeLayoutActivityJava.java
@@ -0,0 +1,18 @@
+package com.codecoderepeat.tutorial04;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Mayur on 10/07/18.
+ */
+
+public class RalativeLayoutActivityJava extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
---
+++ app/src/main/java/com/codecoderepeat/tutorial04/RelativeLayoutActivity.kt
@@ -0,0 +1,15 @@
+package com.codecoderepeat.tutorial04
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by Mayur on 10/07/18.
+ */
+class RelativeLayoutActivity : Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout)
+    }
+}
